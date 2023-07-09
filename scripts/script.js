af47f73ccfd3745553d545529e49c55c151e8958
Jonathan <jonathan.gaul1@gmail.com>
2023-07-09 15:23:45 -0400

    Added functionality to change grid dimensions
    Added the functionality to change the dimensions of the grid
    by using the range slider and apply button

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,73 @@
 createGrid();
-let squares = document.querySelectorAll(".column");
-const backgroundColors = ["yellow", "lime", "green", "orange", "pink", "red", "maroon", "fuchsia",
- "purple", "aqua", "blue", "navy", "grey", "black"]
-let currentColor = 0;
 
-squares.forEach((square) => {
-    square.addEventListener("mouseover", (e) => {
-        if (e.target.style.backgroundColor != "white") {
-            e.target.style.backgroundColor = "white";
-        } else {
-            e.target.style.backgroundColor = backgroundColors[currentColor];
-            currentColor += 1;
-            if(currentColor === backgroundColors.length) {
-                currentColor = 0;
-            }
-        }
-    })
+const backgroundColors = ["yellow", "lime", "green", "orange", "pink", "red", "maroon", "fuchsia",
+    "purple", "aqua", "blue", "navy", "grey", "black"]
+let currentColor = 0;
+const gameContainer = document.querySelector(".game-container");
+const gridSizeSlider = document.querySelector("#grid-size-selector");
+const gridDimensionsText = document.querySelector("#grid-dimensions-text");
+const resizeGridBtn = document.querySelector("#resize-grid-btn");
+let mouseDown = false;
+
+gameContainer.onmousedown = () => {
+    mouseDown = true;
+}
+
+gameContainer.onmouseup = () => {    
+    mouseDown = false;
+}
+
+gridSizeSlider.addEventListener("input", () => {
+    gridDimensionsText.textContent = `${gridSizeSlider.value} x ${gridSizeSlider.value}`;
 });
 
-function createGrid() {
+resizeGridBtn.addEventListener("click", () => {
+    const gridDimensions = parseInt(gridSizeSlider.value);
+    clearGrid();
+    createGrid(gridDimensions);
+    
+});
+
+function clearGrid() {
     const grid = document.querySelector(".game-container");
-    for (let i = 0; i < 16; i++) {
+    stop = grid.childElementCount;
+    for (let i = 0; i < stop; i++) {
+        let row = grid.firstChild;
+        for (let j = 0; j < stop; j++) {
+            row.removeChild(row.firstChild);
+        }
+        grid.removeChild(grid.firstChild);
+    }
+}
+
+function createGrid(gridDimensions) {
+    const dimensions = gridDimensions || 16;
+    const grid = document.querySelector(".game-container");
+    for (let i = 0; i < dimensions; i++) {
         let row = document.createElement("div");
         row.classList.toggle("row");
         grid.appendChild(row);
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < dimensions; j++) {
             let column = document.createElement("div");
             column.classList.toggle("column")
             column.style.backgroundColor = "white";
             row.appendChild(column);
         }
-    } 
+    }
+    addGameBoardListeners();
+    
+}
+function addGameBoardListeners() {
+    let squares = document.querySelectorAll(".column");
+    squares.forEach((square) => {
+        square.addEventListener("mouseover", (e) => {
+            if (mouseDown) {
+                e.target.style.backgroundColor = backgroundColors[currentColor];
+                currentColor += 1;
+                if (currentColor === backgroundColors.length) {
+                    currentColor = 0;
+                }
+            }
+        });
+    });
 }
